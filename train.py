b60786d27676f97005a3c7f0c6b59d2f9cfa34bd
Ved <vedpat3@gmail.com>
2022-04-30 19:06:43 +0530

    updated train.py

--- train.py
+++ train.py
@@ -355,6 +355,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             with amp.autocast(enabled=cuda):
                 print('Images', imgs.shape)
                 pred = model(imgs)  # forward
+                print('Prediction', pred[0].shape)
                 loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
