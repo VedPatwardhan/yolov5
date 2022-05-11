8245847cd513e89541a05ee04dd6af41bc8c3e73
Ved <vedpat3@gmail.com>
2022-05-11 19:05:48 +0530

    Made minor changes

--- captum_code.py
+++ captum_code.py
@@ -28,11 +28,14 @@ class Tensor2Scalar(nn.Module):
 
 
 def captum_attribution(model, im, path):
+    torch.cuda.empty_cache()
     print("File Path: ", path)
     folder_name = path.split('/')[-1][0:-4]
     print("Folder Name", folder_name)
     os.makedirs('./results/{}'.format(folder_name))
+    print(torch.cuda.memory_summary())
     tensor2scalar = Tensor2Scalar(model)
+    torch.cuda.empty_cache()
 
     integrated_gradients = IntegratedGradients(tensor2scalar)
     attributions_ig = integrated_gradients.attribute(im)
@@ -48,6 +51,8 @@ def captum_attribution(model, im, path):
                              cmap=default_cmap,
                              show_colorbar=True)
     fig.savefig('./results/{}/integrated_gradients.png'.format(folder_name))
+    del attributions_ig
+    torch.cuda.empty_cache()
 
     noise_tunnel = NoiseTunnel(integrated_gradients)
     attributions_ig_nt = noise_tunnel.attribute(im, nt_type='smoothgrad_sq')
@@ -59,6 +64,8 @@ def captum_attribution(model, im, path):
                                       cmap=default_cmap,
                                       show_colorbar=True)
     fig.savefig('./results/{}/noise_tunnel.png'.format(folder_name))
+    del noise_tunnel, integrated_gradients, attributions_ig_nt
+    torch.cuda.empty_cache()
 
     rand_img_dist = torch.cat([im * 0, im * 1])
     gradient_shap = GradientShap(tensor2scalar)
@@ -71,14 +78,24 @@ def captum_attribution(model, im, path):
                                       cmap=default_cmap,
                                       show_colorbar=True)
     fig.savefig('./results/{}/gradient_shap.png'.format(folder_name))
+    del gradient_shap, attributions_gs
+    torch.cuda.empty_cache()
 
-    occlusion = Occlusion(tensor2scalar)
-    attributions_occ = occlusion.attribute(im, sliding_window_shapes=(3,15, 15))
-    print("Attributions Occlusion", attributions_occ.shape)
-    fig, ax = viz.visualize_image_attr_multiple(np.transpose(attributions_occ.squeeze().cpu().detach().numpy(), (1,2,0)),
-                                      np.transpose(im.squeeze().cpu().detach().numpy(), (1,2,0)),
-                                      ["original_image", "heat_map"],
-                                      ["all", "positive"],
-                                      show_colorbar=True,
-                                      outlier_perc=2)
-    fig.savefig('./results/{}/occlusion.png'.format(folder_name))
+    # occlusion = Occlusion(tensor2scalar)
+    # attributions_occ = occlusion.attribute(im, sliding_window_shapes=(3,15, 15))
+    # print("Attributions Occlusion", attributions_occ.shape)
+    # fig, ax = viz.visualize_image_attr_multiple(np.transpose(attributions_occ.squeeze().cpu().detach().numpy(), (1,2,0)),
+    #                                   np.transpose(im.squeeze().cpu().detach().numpy(), (1,2,0)),
+    #                                   ["original_image", "heat_map"],
+    #                                   ["all", "positive"],
+    #                                   show_colorbar=True,
+    #                                   outlier_perc=2)
+    # fig.savefig('./results/{}/occlusion.png'.format(folder_name))
+    # del occlusion, attributions_occ
+    # torch.cuda.empty_cache()
+
+    del tensor2scalar
+    torch.cuda.empty_cache()
+
+    print(torch.cuda.memory_summary())
+    
